96381c62bc8ea5cdac42550115ff8c66d2810cf1
FlyingElephant135 <46475082+FlyingElephant135@users.noreply.github.com>
2023-10-24 21:16:31 +0300

    commit

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -1,10 +1,32 @@
 package com.epam.collections.queue;
 
 import java.util.ArrayDeque;
+import java.util.Objects;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> deck = new ArrayDeque<>();
+        for (int i = 0; i < 2; i++) {
+            deck.add(Objects.requireNonNull(firstQueue.poll()));
+        }
+        for (int i = 0; i < 2; i++) {
+            deck.add(Objects.requireNonNull(secondQueue.poll()));
+        }
+
+        while (!firstQueue.isEmpty() && !secondQueue.isEmpty())
+        {
+            firstQueue.add(deck.pollLast());
+            for (int i = 0; i < 2; i++) {
+                deck.addLast(Objects.requireNonNull(firstQueue.poll()));
+            }
+            secondQueue.add(deck.pollLast());
+            for (int i = 0; i < 2; i++) {
+                deck.addLast(Objects.requireNonNull(secondQueue.poll()));
+            }
+        }
+
+        return deck;
     }
 }
